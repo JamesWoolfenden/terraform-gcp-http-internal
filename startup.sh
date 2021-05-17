25534bb4f45dd5a3172cf3b2a5da37f3b84423ea
James Woolfenden <James.Woolfenden@gmail.com>
2021-05-17 08:42:44 +0100

    pass linter

--- startup.sh
+++ startup.sh
@@ -2,10 +2,10 @@
 
 apt-get update
 apt-get install -y apache2 php
-cd /var/www/html
+cd /var/www/html || exit
 rm index.html -f
 rm index.php -f
 wget https://storage.googleapis.com/cloudnet-lab/index.php
 META_REGION_STRING=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google")
-REGION=`echo "$META_REGION_STRING" | awk -F/ '{print $4}'`
+REGION=$(echo "$META_REGION_STRING" | awk -F/ '{print $4}')
 sed -i "s|region-here|$REGION|" index.php
